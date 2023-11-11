9cb17dd5404f6c2eed79ffb89c2b4968dd8a961c
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-11-10 21:58:40 -0800

    added User-Blog connections

--- models/User.js
+++ models/User.js
@@ -1,17 +1,3 @@
-const User = require('./User');
-const BlogPost = require('./BlogPost');
-
-User.hasMany(BlogPost, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-BlogPost.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-module.exports = { User, BlogPost };
-
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
